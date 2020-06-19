2401e565a4022a315bb85aa177151e0f33dd2da7
Abhilash KP <abhilashmhaisne@gmail.com>
2020-06-20 00:41:57 +0530

    Changes to Parser code

--- unifilter.py
+++ unifilter.py
@@ -1,5 +1,6 @@
 import sys
 import re
+import requests
 
 def deltags(buf):
 #Deletes all HTML tags and gives back content inside
@@ -25,7 +26,24 @@ while ind < len(lines):
         if unirankstr.startswith("#"):
             unirank = unirankstr.split(" ")[0].split("#")[1]
             unidict[unirank] = lineCont
-            resfile.write(lineCont + ": " + str(unirank) + "\n")
+            #Get Fees
+            #query = query.replace(" ","+")
+            queryfees = "https://search.prtl.co/2018-07-23/?q=en-283%7Ckw-" + lineCont + "%7Clv-master%7Cmh-face2face%7Ctc-EUR"
+
+            #r = request.urlopen(query)
+            try:
+                r = requests.get(queryfees)
+                if("tuition_fee" in r.json()[0]):
+                    fee = r.json()[0]["tuition_fee"]
+                    feeval= fee["value"]
+                    feefreq = fee["unit"]
+                    feecurr = fee["currency"]
+                    feestr = str(feeval) + " " + feecurr + " per " + feefreq 
+                else:
+                    feestr = "Not Provided"
+            except IndexError:
+                feestr = "Not Provided"
+        resfile.write(lineCont + ";" + str(unirank) + ";" + feestr + "\n")
         ind = ind + 3
     else:
         ind = ind + 1
